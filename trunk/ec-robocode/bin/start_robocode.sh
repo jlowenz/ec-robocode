61c6a4c5a2bd2ba8b6f9ef93493c28cc9e1897b5
ron1968 <ron1968@2b9fc8c8-3420-0410-9b65-e3934321462e>
2006-11-12 01:40:40 +0000

    Made settings settable

--- trunk/ec-robocode/bin/start_robocode.sh
+++ trunk/ec-robocode/bin/start_robocode.sh
@@ -1,14 +1,22 @@
 #!/bin/sh
 
+if [[ -z ""{$GP_HOME} ]] 
+then
+	echo "Must set GP_HOME"
+	exit
+fi
 
-export CLASSPATH=".:build/gp.jar"
+export CLASSPATH="${GP_HOME}"
 
-for JAR in 	rc.jar jsk-dl.jar rio.jar jscience.jar \
+for JAR in 	gp.jar rc.jar jsk-dl.jar rio.jar jscience.jar \
 			functionalj-0.8-jdk15.jar jsk-policy.jar jsk-lib.jar \
 			jsk-platform.jar jsk-resources.jar junit-4.1.jar
 do
-	export CLASSPATH="${CLASSPATH}:build/${JAR}"
+	export CLASSPATH="${CLASSPATH}:${GP_HOME}/build/${JAR}"
 done
 echo $CLASSPATH
 
-java -Djava.security.policy=/home/rbowers/.java.policy -Dorg.jini.rio.groups="GPRobocode" -cp "${CLASSPATH}" -jar build/gp.jar -battle config/sample.battle
+java -Djava.security.policy=/home/rbowers/.java.policy \
+	-DGP_HOME="${GP_HOME}"  \
+	-Dorg.jini.rio.groups="GPRobocode"  \
+	-cp "${CLASSPATH}" -jar ${GP_HOME}/build/gp.jar -battle ${GP_HOME}/config/sample.battle

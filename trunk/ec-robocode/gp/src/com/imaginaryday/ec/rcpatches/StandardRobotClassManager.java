0012e722e1687872ccda10c9f6e88b2a102e5a9a
ron1968 <ron1968@2b9fc8c8-3420-0410-9b65-e3934321462e>
2006-11-12 20:12:21 +0000

    stuff

---
+++ trunk/ec-robocode/gp/src/com/imaginaryday/ec/rcpatches/StandardRobotClassManager.java
@@ -0,0 +1,36 @@
+package com.imaginaryday.ec.rcpatches;
+
+import robocode.peer.robot.RobotClassManager;
+import robocode.peer.TeamPeer;
+import robocode.repository.RobotSpecification;
+import robocode.Robot;
+import robocode.AdvancedRobot;
+
+/**
+ * @author rbowers
+ *         Date: Nov 12, 2006
+ *         Time: 1:57:51 PM
+ */
+public class StandardRobotClassManager extends RobotClassManager {
+
+    private Class<? extends Robot> robotClass;
+    private RobotSpecification robotSpecification;
+
+    public StandardRobotClassManager(RobotSpecification robotSpecification,
+                                     Class<? extends Robot> clazz) {
+        super(robotSpecification);
+        this.robotClass = clazz;
+        this.robotSpecification = robotSpecification;
+    }
+
+    @Override
+    public Robot getRobotInstance() throws IllegalAccessException, InstantiationException {
+        Robot robot;
+        robot = (Robot) (getRobotClass().newInstance());
+        return robot;
+    }
+
+    public RobotSpecification getRobotSpecification() {
+        return this.robotSpecification;
+    }
+}

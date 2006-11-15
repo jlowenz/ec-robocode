27a3a5a68f3fdcf008e2561978e8080f99debb49
jlowens <jlowens@2b9fc8c8-3420-0410-9b65-e3934321462e>
2006-11-15 17:40:02 +0000

    divide by zero sanitization

--- ec-robocode/gp/src/com/imaginaryday/ec/gp/nodes/Divide.java
+++ ec-robocode/gp/src/com/imaginaryday/ec/gp/nodes/Divide.java
@@ -3,6 +3,8 @@ package com.imaginaryday.ec.gp.nodes;
 import com.imaginaryday.ec.gp.AbstractNode;
 import com.imaginaryday.ec.gp.Node;
 
+import java.util.Random;
+
 /**
  * <b>
  * User: jlowens<br>
@@ -11,7 +13,7 @@ import com.imaginaryday.ec.gp.Node;
  * </b>
  */
 public class Divide extends AbstractNode {
-
+    private static Random rand = new Random();
     private Node[] operands = new Node[2];
 
     protected Node[] children() {
@@ -30,15 +32,19 @@ public class Divide extends AbstractNode {
         return Number.class;
     }
 
+    private boolean closeToZero(double val) {
+        if (Math.abs(val) < 0.000001) return true;
+        return false;
+    }
 
     public Object evaluate() {
         double x = ((Number)operands[0].evaluate()).doubleValue();
         double y = ((Number)operands[1].evaluate()).doubleValue();
-	    assert !(Double.isNaN(x) || Double.isInfinite(x)) : "x is bad. from: " + operands[0];
-	    assert !(Double.isNaN(y) || Double.isInfinite(y)) : "y is bad. from: " + operands[1];
-        if (y == 0.0 || y == -0.0) return 0.0;
+        assert !(Double.isNaN(x) || Double.isInfinite(x)) : "x is bad. from: " + operands[0];
+        assert !(Double.isNaN(y) || Double.isInfinite(y)) : "y is bad. from: " + operands[1];
+        if (closeToZero(y)) return (rand.nextBoolean()) ? 1.0 : 0.0; // Ha Ha - I'm an idiot :-)
         double result = x / y;
-	    assert !(Double.isNaN(result) || Double.isInfinite(result)) : "division result is bad";
-	    return x / y;
+        assert !(Double.isNaN(result) || Double.isInfinite(result)) : "division result is bad";
+        return x / y;
     }
 }

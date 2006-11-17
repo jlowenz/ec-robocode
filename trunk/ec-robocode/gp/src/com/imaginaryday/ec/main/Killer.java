d9f5ecab858e5ce2bb84aadb3195f48998be5f0f
ron1968 <ron1968@2b9fc8c8-3420-0410-9b65-e3934321462e>
2006-11-17 14:41:42 +0000

    Worker killer

---
+++ trunk/ec-robocode/gp/src/com/imaginaryday/ec/main/Killer.java
@@ -0,0 +1,40 @@
+package com.imaginaryday.ec.main;
+
+import net.jini.space.JavaSpace;
+import net.jini.core.entry.Entry;
+import com.imaginaryday.util.SpaceFinder;
+import com.imaginaryday.util.PoisonPill;
+
+/**
+ * @author Ronald A. Bowers
+ * @version 1.0
+ */
+public class Killer {
+
+
+    public static void main(String[] args) {
+        String id = null;
+        if (args.length != 1) {
+            System.out.println("Usage: com.imaginaryday.ec.main.killer worker_id");
+            System.exit(1);
+        } else {
+            id = args[0];
+        }
+        Killer killer = new Killer(id);
+
+    }
+
+    private Killer(String id) {
+        try {
+            JavaSpace space = new SpaceFinder().getSpace();
+            Entry pill = new PoisonPill(id);
+            space.write(pill, null, 120000);
+            System.out.println(new StringBuilder().append("Submitted task to kill ")
+                    .append(id).toString());
+        } catch (Exception e) {
+            e.printStackTrace();  //Todo change body of catch statement use File | Settings | File Templates.
+        }
+
+    }
+
+}

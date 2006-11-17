46e31951156c5a03ddd7d2e1c8dd866a040c954f
ron1968 <ron1968@2b9fc8c8-3420-0410-9b65-e3934321462e>
2006-11-17 17:14:29 +0000

    Shutdown support

---
+++ ec-robocode/bin/killer.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+if [[ -z ""{$GP_HOME} ]]
+then
+	echo "Must set GP_HOME"
+	exit
+fi
+
+export CLASSPATH="${GP_HOME}"
+
+for JAR in 	gp.jar rc.jar jsk-dl.jar rio.jar jscience.jar \
+			functionalj-0.8-jdk15.jar jsk-policy.jar jsk-lib.jar \
+			jsk-platform.jar jsk-resources.jar junit-4.1.jar
+do
+	export CLASSPATH="${CLASSPATH}:${GP_HOME}/build/${JAR}"
+done
+export CLASSPATH="${CLASSPATH}:${GP_HOME}/rc/classes/"
+echo $CLASSPATH
+
+java -Djava.security.policy=/home/rbowers/.java.policy \
+	-DGP_HOME="${GP_HOME}"  \
+	-server \
+	-Dorg.jini.rio.groups="GPRobocode"  \
+	-cp "${CLASSPATH}" com.imaginaryday.ec.main.Killer $*

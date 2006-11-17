663098f8793eebd62a440a232ed4991bdee9028c
ron1968 <ron1968@2b9fc8c8-3420-0410-9b65-e3934321462e>
2006-11-17 17:11:41 +0000

    Shutdown support

--- trunk/ec-robocode/gp/src/com/imaginaryday/util/PoisonPill.java
+++ trunk/ec-robocode/gp/src/com/imaginaryday/util/PoisonPill.java
@@ -10,11 +10,32 @@ public class PoisonPill implements Entry {
     public String id;
 
     public PoisonPill() {
-        
+
     }
 
     public PoisonPill(String id) {
         this.id = id;
     }
 
+
+    public String toString() {
+        return "PoisonPill{" +
+                "id='" + id + '\'' +
+                '}';
+    }
+
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        final PoisonPill that = (PoisonPill) o;
+
+        if (id != null ? !id.equals(that.id) : that.id != null) return false;
+
+        return true;
+    }
+
+    public int hashCode() {
+        return (id != null ? id.hashCode() : 0);
+    }
 }

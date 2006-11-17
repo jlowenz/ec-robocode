d7e6f1244433772a4a9f9e88495c6026f44354a1
ron1968 <ron1968@2b9fc8c8-3420-0410-9b65-e3934321462e>
2006-11-17 16:38:12 +0000

    Added args suuport

--- ec-robocode/bin/start_robocode.sh
+++ ec-robocode/bin/start_robocode.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [[ -z ""{$GP_HOME} ]] 
+if [[ -z ""{$GP_HOME} ]]
 then
 	echo "Must set GP_HOME"
 	exit
@@ -19,6 +19,6 @@ echo $CLASSPATH
 
 java -Djava.security.policy=/home/rbowers/.java.policy \
 	-DGP_HOME="${GP_HOME}"  \
-	-ea -server \
+	-server \
 	-Dorg.jini.rio.groups="GPRobocode"  \
-	-cp "${CLASSPATH}" -jar ${GP_HOME}/build/gp.jar -battle ${GP_HOME}/config/sample.battle
+	-cp "${CLASSPATH}" -jar ${GP_HOME}/build/gp.jar -battle ${GP_HOME}/config/sample.battle $*

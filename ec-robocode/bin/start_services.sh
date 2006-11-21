2fe599eed7d9c9b7584b4d005256cf458266e172
ron1968 <ron1968@2b9fc8c8-3420-0410-9b65-e3934321462e>
2006-11-21 12:57:18 +0000

    services now loads logging file.

--- ec-robocode/bin/start_services.sh
+++ ec-robocode/bin/start_services.sh
@@ -4,5 +4,6 @@ set -x
 
 java -Djava.security.policy=/home/rbowers/.java.policy	\
 	-DGP_HOME=${GP_HOME} \
+	-Djava.util.logging.config.file=${GP_HOME}/config/jini.logging \
      -jar ${GP_HOME}/gp/lib/jini/start.jar	\
      ${GP_HOME}/config/start-services.config

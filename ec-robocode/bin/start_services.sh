6e16a1a46b0e83379198acbd8aae34d0b15fe7b5
ron1968 <ron1968@2b9fc8c8-3420-0410-9b65-e3934321462e>
2006-11-21 13:12:09 +0000

    Enlarged heap!

--- ec-robocode/bin/start_services.sh
+++ ec-robocode/bin/start_services.sh
@@ -3,6 +3,7 @@
 set -x
 
 java -Djava.security.policy=/home/rbowers/.java.policy	\
+    -Xmx1G \
 	-DGP_HOME=${GP_HOME} \
 	-Djava.util.logging.config.file=${GP_HOME}/config/jini.logging \
      -jar ${GP_HOME}/gp/lib/jini/start.jar	\

efc1911fd8febac3cb5c12b00407080299a90f9a
ron1968 <ron1968@2b9fc8c8-3420-0410-9b65-e3934321462e>
2006-11-21 13:15:42 +0000

    jmx

--- ec-robocode/bin/start_services.sh
+++ ec-robocode/bin/start_services.sh
@@ -5,6 +5,7 @@ set -x
 java -Djava.security.policy=/home/rbowers/.java.policy	\
     -Xmx1G \
 	-DGP_HOME=${GP_HOME} \
+	-Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.authenticate=false \
 	-Djava.util.logging.config.file=${GP_HOME}/config/jini.logging \
      -jar ${GP_HOME}/gp/lib/jini/start.jar	\
      ${GP_HOME}/config/start-services.config

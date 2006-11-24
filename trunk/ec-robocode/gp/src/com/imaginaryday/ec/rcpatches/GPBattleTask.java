c03c02f7c0e5018316c1acea99b36a07a8ee0fce
ron1968 <ron1968@2b9fc8c8-3420-0410-9b65-e3934321462e>
2006-11-24 22:22:56 +0000

    Changed to wrap Nodes in marshalled objects

--- trunk/ec-robocode/gp/src/com/imaginaryday/ec/rcpatches/GPBattleTask.java
+++ trunk/ec-robocode/gp/src/com/imaginaryday/ec/rcpatches/GPBattleTask.java
@@ -4,6 +4,9 @@ import com.imaginaryday.ec.gp.Node;
 import com.imaginaryday.ec.main.Member;
 import net.jini.core.entry.Entry;
 
+import java.rmi.MarshalledObject;
+import java.io.IOException;
+
 /**
  * @author rbowers
  *         Date: Nov 2, 2006
@@ -16,15 +19,15 @@ public class GPBattleTask implements Entry {
     public String robot1;
     public String robot2;
 
-    public Node moveProgram1;
-    public Node turretProgram1;
-    public Node radarProgram1;
-    public Node shootProgram1;
+    public MarshalledObject moveProgram1;
+    public MarshalledObject turretProgram1;
+    public MarshalledObject radarProgram1;
+    public MarshalledObject shootProgram1;
 
-    public Node moveProgram2;
-    public Node turretProgram2;
-    public Node radarProgram2;
-    public Node shootProgram2;
+    public MarshalledObject moveProgram2;
+    public MarshalledObject turretProgram2;
+    public MarshalledObject radarProgram2;
+    public MarshalledObject shootProgram2;
 
     public Boolean done = Boolean.FALSE;
 
@@ -33,16 +36,20 @@ public class GPBattleTask implements Entry {
         this.battle = battle;
 
         robot1 = member1.getName();
-        moveProgram1 = member1.getMoveProgram();
-        turretProgram1 = member1.getTurretProgram();
-        radarProgram1 = member1.getRadarProgram();
-        shootProgram1 = member1.getShootProgram();
+        try {
+            moveProgram1 = new MarshalledObject(member1.getMoveProgram());
+            turretProgram1 = new MarshalledObject(member1.getTurretProgram());
+            radarProgram1 = new MarshalledObject(member1.getRadarProgram());
+            shootProgram1 = new MarshalledObject(member1.getShootProgram());
 
-        robot2 = member2.getName();
-        moveProgram2 = member2.getMoveProgram();
-        turretProgram2 = member2.getTurretProgram();
-        radarProgram2 = member2.getRadarProgram();
-        shootProgram2 = member2.getShootProgram();
+            robot2 = member2.getName();
+            moveProgram2 = new MarshalledObject(member2.getMoveProgram());
+            turretProgram2 = new MarshalledObject(member2.getTurretProgram());
+            radarProgram2 = new MarshalledObject(member2.getRadarProgram());
+            shootProgram2 = new MarshalledObject(member2.getShootProgram());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public GPBattleTask() {
@@ -54,10 +61,14 @@ public class GPBattleTask implements Entry {
         this.battle = battle;
 
         robot1 = member1.getName();
-        moveProgram1 = member1.getMoveProgram();
-        turretProgram1 = member1.getTurretProgram();
-        radarProgram1 = member1.getRadarProgram();
-        shootProgram1 = member1.getShootProgram();
+        try {
+            moveProgram1 = new MarshalledObject(member1.getMoveProgram());
+            turretProgram1 = new MarshalledObject(member1.getTurretProgram());
+            radarProgram1 = new MarshalledObject(member1.getRadarProgram());
+            shootProgram1 = new MarshalledObject(member1.getShootProgram());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         robot2 = standardBotName;
         moveProgram2 = null;
@@ -78,10 +89,118 @@ public class GPBattleTask implements Entry {
         return new StringBuilder().append("GPBattleTask{").append("battle=").append(battle)
                 .append(", generation=").append(generation).append(", robot1='").append(robot1)
                 .append('\'').append(", robot2='").append(robot2).append('\'').append(", moveProgram1=")
-                .append(moveProgram1).append(", turretProgram1=").append(turretProgram1).append(", radarProgram1=")
-                .append(radarProgram1).append(", shootProgram1=").append(shootProgram1).append(", moveProgram2=")
-                .append(moveProgram2).append(", turretProgram2=").append(turretProgram2).append(", radarProgram2=")
-                .append(radarProgram2).append(", shootProgram2=").append(shootProgram2).append(", done=")
+                .append(getMoveProgram1()).append(", turretProgram1=").append(getTurretProgram1()).append(", radarProgram1=")
+                .append(getRadarProgram1()).append(", shootProgram1=").append(getShootProgram1()).append(", moveProgram2=")
+                .append(getMoveProgram2()).append(", turretProgram2=").append(getTurretProgram2()).append(", radarProgram2=")
+                .append(getRadarProgram2()).append(", shootProgram2=").append(getShootProgram2()).append(", done=")
                 .append(done).append('}').toString();
+    }
+
+    public Integer getBattle() {
+        return battle;
+    }
+
+    public Boolean getDone() {
+        return done;
+    }
+
+    public Integer getGeneration() {
+        return generation;
+    }
+
+    public Node getMoveProgram1() {
+        try {
+            return (Node)moveProgram1.get();
+        } catch (IOException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+        return null;
+    }
+
+    public Node getMoveProgram2() {
+        try {
+            return (Node) moveProgram2.get();
+        } catch (IOException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+        return null;
+    }
+
+    public Node getRadarProgram1() {
+        try {
+            return (Node) radarProgram1.get();
+        } catch (IOException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+        return null;
+    }
+
+    public Node getRadarProgram2() {
+        try {
+            return (Node)radarProgram2.get();
+        } catch (IOException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+        return null;
+    }
+
+    public String getRobot1() {
+        return robot1;
+    }
+
+    public String getRobot2() {
+        return robot2;
+    }
+
+    public Node getShootProgram1() {
+        try {
+            return (Node)shootProgram1.get();
+        } catch (IOException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+        return null;
+    }
+
+    public Node getShootProgram2() {
+        try {
+            return (Node)shootProgram2.get();
+        } catch (IOException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+        return null;
+    }
+
+    public Node getTurretProgram1() {
+        try {
+            return (Node)turretProgram1.get();
+        } catch (IOException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+        return null;
+    }
+
+    public Node getTurretProgram2() {
+        try {
+            return (Node) turretProgram2.get();
+        } catch (IOException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+        return null;
     }
 }

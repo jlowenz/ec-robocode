f7d206ed3cd57af42117b15209a3a1a6567961c1
ron1968 <ron1968@2b9fc8c8-3420-0410-9b65-e3934321462e>
2006-11-28 23:41:25 +0000

    NEW!!!! IMPROVED!!!!

--- ec-robocode/gp/src/com/imaginaryday/ec/rcpatches/GPFitnessCalc.java
+++ ec-robocode/gp/src/com/imaginaryday/ec/rcpatches/GPFitnessCalc.java
@@ -48,7 +48,7 @@ public class GPFitnessCalc {
         // Add a bonus for accuracy
         double accuracy;
         if (robot.getNumBulletsFired() > 0) {
-            accuracy = opponent.getNumBulletHits() / robot.getNumBulletsFired();
+            accuracy = (double) robot.getNumBulletHits() / (double) robot.getNumBulletsFired();
         } else {
             accuracy = 0;
         }
@@ -60,7 +60,7 @@ public class GPFitnessCalc {
          */
         double scanEfficiency;
         if (robot.getNumScanEvents() > 0 && robot.getScanRadians() > 0.0) {
-            scanEfficiency = (((double)robot.getNumScanEvents() / (robot.getScanRadians() / (2.0 * Math.PI))) - 1.0);
+            scanEfficiency = (((double) robot.getNumScanEvents() / (robot.getScanRadians() / (2.0 * Math.PI))) - 1.0);
             scanEfficiency /= 8.0; // scale by 1/8 so that we need to get 8 events per rotation to get a _good_ bonus
         } else {
             scanEfficiency = 0.0;
@@ -68,18 +68,22 @@ public class GPFitnessCalc {
         double scanEfficiencyBonus = scanEfficiency * robot.getBulletDamage();
 
         double damageFitness = robot.getBulletDamage() + robot.getBulletDamageBonus() + robot.getRamDamage()
-                + robot.getRamDamageBonus() + accuracyBonus + scanEfficiencyBonus
-                - opponent.getBulletDamage() - opponent.getRamDamage() ;
+                + robot.getRamDamageBonus() + accuracyBonus + scanEfficiencyBonus;
 
         double d = (robot.getDistanceTravelled() < 1) ? 1.0 : robot.getDistanceTravelled();
-        double b = (robot.getNumBulletsFired() < 1) ? 1.0 : robot.getNumBulletHits();
+        double b = (robot.getNumBulletsFired() < 1) ? 1.0 : robot.getNumBulletsFired();
         double s = (robot.getNumScanEvents() < 1) ? 1.0 : robot.getNumScanEvents();
 
-        double tweekFitness = Math.log(d/10.0) + Math.log(b) + Math.log(s);
+        double tweekFitness = Math.log(d) + Math.log(b) + Math.log(s);
 
-        double winningFraction = (double)robot.getFirsts() / 10.0;
+        double winningFraction = (double) robot.getFirsts() / 10.0;
+        double doSomethingBonus;
+        if ((robot.getDistanceTravelled() > 0 ||
+                robot.getNumBulletsFired() > 0 ||
+                robot.getNumScanEvents() > 0)) doSomethingBonus = 5.0;
+        else doSomethingBonus = 0.0;
 
-        return  winningFraction * (damageFitness + tweekFitness);
+        return winningFraction * (damageFitness + tweekFitness) + doSomethingBonus;
     }
 
 }

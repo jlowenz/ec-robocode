df80a53996ee8838025c86a8e7c3c7df2096347b
jlowens <jlowens@2b9fc8c8-3420-0410-9b65-e3934321462e>
2006-12-03 02:27:56 +0000

    Adjusted scripts to support alternate rsh methods (for home testing). Altered Driver to support "random" generations, where it generates a full population of random bots (like pop 0), then picks the top bot and carries it through to the next random generation, until the nth generation contains only the best bots of num_pop generations.

--- ec-robocode/bin/distribute.sh
+++ ec-robocode/bin/distribute.sh
@@ -4,6 +4,11 @@
 MACHINES=$1
 NUMPROCS=$2
 
+if [[ -z ""$GP_RSH ]]; then
+    GP_RCP=krcp
+    GP_RSH=krsh
+fi
+
 if [[ -z ""$2 ]]; then
     NUMPROCS=4
 fi
@@ -20,6 +25,6 @@ tar czf ecdist.tgz ecdist
 
 for m in $MACHINES; do
     echo $m
-    krcp ecdist.tgz $m:/tmp
-    krsh $m "cd /tmp; tar xzf ecdist.tgz; cd ecdist; chmod +x bin/start_robocode.sh; source ~/.bashrc; GP_HOME=/tmp/ecdist nice -n20 bin/start_robocode.sh $NUMPROCS $m$USER" &
+    $GP_RCP ecdist.tgz $m:/tmp
+    $GP_RSH $m "cd /tmp; tar xzf ecdist.tgz; cd ecdist; chmod +x bin/start_robocode.sh; GP_HOME=/tmp/ecdist nice -n20 bin/start_robocode.sh $NUMPROCS $m$USER" &
 done

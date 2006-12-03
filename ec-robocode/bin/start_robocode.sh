f2ecaf059a4e9e513f0d26b052df5a26ad1cbf26
jlowens <jlowens@2b9fc8c8-3420-0410-9b65-e3934321462e>
2006-12-03 04:50:22 +0000

    trying yet again

--- ec-robocode/bin/start_robocode.sh
+++ ec-robocode/bin/start_robocode.sh
@@ -14,21 +14,7 @@ echo "GP_HOME is ${GP_HOME}"
 if [ -z ""$JAVA_HOME ]
 then
 	echo "JAVA_HOME is not set properly: $JAVA_HOME"
-	echo "trying to source bashrc..."
-	. $HOME/.bashrc
-	if [ -z ""$JAVA_HOME ] 
-	then
-		echo "Nope, didn't work, trying path on system: $HOSTNAME"
-		if [ `uname` = "Darwin" ]; then
-		    JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Home
-		else
-		    echo "giving up"
-		    exit;
-		fi
-	fi
 fi
-
-export PATH=$JAVA_HOME/bin:$PATH
 
 export CLASSPATH="${GP_HOME}"
 
@@ -42,7 +28,7 @@ export CLASSPATH="${CLASSPATH}"
 i=0
 while [ $i -lt $PARALLEL ]; do
     echo "starting on $HOSTNAME..."
-    nice -n20 java -Djava.security.policy=${GP_HOME}/bin/.java.policy \
+    nice -n20 $JAVA_HOME/bin/java -Djava.security.policy=${GP_HOME}/bin/.java.policy \
         -Djava.util.logging.config.file=${GP_HOME}/config/jini.logging \
 	    -DGP_HOME="${GP_HOME}"  \
 	    -server \

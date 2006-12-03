f9eb36dcd5e5f7c448f64ff275f46b8c135ea915
jlowens <jlowens@2b9fc8c8-3420-0410-9b65-e3934321462e>
2006-12-03 04:38:53 +0000

    trying yet again

--- trunk/ec-robocode/bin/distribute.sh
+++ trunk/ec-robocode/bin/distribute.sh
@@ -29,5 +29,5 @@ echo "GP_RCP=$GP_RCP"
 for m in $MACHINES; do
     echo $m
     $GP_RCP ecdist.tgz $m:/tmp
-    $GP_RSH $m "cd /tmp; tar xzf ecdist.tgz; cd ecdist; chmod +x bin/start_robocode.sh; GP_HOME=/tmp/ecdist nice -n20 bin/start_robocode.sh $NUMPROCS $m$USER" &
+    $GP_RSH $m "cd /tmp; tar xzf ecdist.tgz; cd ecdist; chmod +x bin/start_robocode.sh; export GP_HOME=/tmp/ecdist; nice -n20 bin/start_robocode.sh $NUMPROCS $m$USER" &
 done

869538e2a05e3a4447456d54b3bca9d5bf573380
jlowens <jlowens@2b9fc8c8-3420-0410-9b65-e3934321462e>
2006-12-03 04:07:49 +0000

    trying yet again

--- trunk/ec-robocode/bin/start_robocode.sh
+++ trunk/ec-robocode/bin/start_robocode.sh
@@ -18,7 +18,7 @@ then
 	if [ -z ""$JAVA_HOME ] 
 	then
 		echo "Nope, didn't work, trying path on system: $HOSTNAME"
-		if [ `uname` = "Darwin ]; then
+		if [ `uname` = "Darwin" ]; then
 		    JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Home
 		else
 		    echo "giving up"

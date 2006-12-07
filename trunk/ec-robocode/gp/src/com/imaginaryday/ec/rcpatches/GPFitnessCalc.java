9e4c295cf16c079fb289a60fd093869fb6e1260d
jlowens <jlowens@2b9fc8c8-3420-0410-9b65-e3934321462e>
2006-12-07 15:29:14 +0000

    Configuration for run 6. Altered fitness for survival bonus, removed short battle bonus

--- trunk/ec-robocode/gp/src/com/imaginaryday/ec/rcpatches/GPFitnessCalc.java
+++ trunk/ec-robocode/gp/src/com/imaginaryday/ec/rcpatches/GPFitnessCalc.java
@@ -12,8 +12,61 @@ public class GPFitnessCalc {
     private static final double EFFICIENCY_SCALE = 4.0;
     private static double minBattleLength = Double.MAX_VALUE;
     private static final double SHORT_BATTLE_SCALE = 1.1;
+	private static final double SURVIVAL_SCALE = 2.0;
 
-    public static double getFitness(int numGenerations, RobotResults robot, RobotResults opponent) {
+	public static double getFitness(int numGenerations, RobotResults robot, RobotResults opponent)
+	{
+		return getScanAndSurviveFitness(numGenerations, robot, opponent);
+	}
+
+	private static double getScanAndSurviveFitness(int numGenerations, RobotResults robot, RobotResults opponent) {
+
+	    // Add a bonus for accuracy
+	    double accuracy;
+	    if (robot.getNumBulletsFired() > 0) {
+	        accuracy = (double) robot.getNumBulletHits() / (double) robot.getNumBulletsFired();
+	    } else {
+	        accuracy = 0;
+	    }
+	    double accuracyBonus = ACCURACY_SCALE * accuracy * robot.getBulletDamage();
+
+	    /*
+	     * Add a bonus for scan efficiency.
+	     * Efficiency is getting one scan even per battle "tick"
+	     */
+	    double scanEfficiency;
+	    if (robot.getNumScanEvents() > 0 && robot.getBattleLength() > 0) {
+	        double revs = robot.getBattleLength();
+	        scanEfficiency = (double) robot.getNumScanEvents() / revs;
+	    } else {
+	        scanEfficiency = 0.0;
+	    }
+	    double scanEfficiencyBonus = EFFICIENCY_SCALE * scanEfficiency * robot.getBulletDamage();
+
+		// bonus for survival
+		double survialBonus = SURVIVAL_SCALE * robot.getEnergy() * 0.01 * robot.getBulletDamage(); // they start with 100,
+
+	    double damageFitness = robot.getBulletDamage() + robot.getBulletDamageBonus() + robot.getRamDamage()
+	            + robot.getRamDamageBonus() + accuracyBonus + scanEfficiencyBonus + survialBonus;
+
+	    double d = (robot.getDistanceTravelled() < 1) ? 1.0 : robot.getDistanceTravelled();
+	    double b = (robot.getNumBulletsFired() < 1) ? 1.0 : robot.getNumBulletsFired();
+	    double s = (robot.getNumScanEvents() < 1) ? 1.0 : robot.getNumScanEvents();
+
+	    double tweekFitness = Math.log(d) + Math.log(b) + Math.log(s);
+
+	    double winningFraction = (double) robot.getFirsts() / 10.0;
+	    double doSomethingBonus;
+	    if ((robot.getDistanceTravelled() > 0 ||
+	            robot.getNumBulletsFired() > 0 ||
+	            robot.getNumScanEvents() > 0)) doSomethingBonus = 5.0;
+	    else doSomethingBonus = 0.0;
+
+	    return winningFraction * (damageFitness + tweekFitness) + doSomethingBonus;
+	}
+
+
+    private static double getMinBattleFitness(int numGenerations, RobotResults robot, RobotResults opponent) {
 
         // Add a bonus for accuracy
         double accuracy;

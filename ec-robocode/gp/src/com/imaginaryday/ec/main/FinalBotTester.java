6e7a81c6b5ca1fe1f84e7cc40ebbec129b69e48a
ron1968 <ron1968@2b9fc8c8-3420-0410-9b65-e3934321462e>
2006-12-08 16:37:46 +0000

    Added human-coded bots for final performance test

---
+++ ec-robocode/gp/src/com/imaginaryday/ec/main/FinalBotTester.java
@@ -0,0 +1,316 @@
+package com.imaginaryday.ec.main;
+
+import com.imaginaryday.ec.rcpatches.GPBattleResults;
+import com.imaginaryday.ec.rcpatches.GPBattleTask;
+import net.jini.core.entry.Entry;
+import net.jini.core.entry.UnusableEntryException;
+import net.jini.core.lease.Lease;
+import net.jini.core.lease.LeaseDeniedException;
+import net.jini.core.transaction.Transaction;
+import net.jini.core.transaction.TransactionException;
+import net.jini.core.transaction.TransactionFactory;
+import net.jini.core.transaction.server.TransactionManager;
+import net.jini.space.JavaSpace;
+
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Logger;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: rbowers
+ * Date: Nov 14, 2006
+ * Time: 5:35:22 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class FinalBotTester {
+
+    private JavaSpace space;
+    private final TransactionManager transactionManager;
+    private Logger logger = Logger.getLogger(this.getClass().getName());
+    private Writer summaryWriter;
+    private Writer detailsWriter;
+    private static final int MAX_TAKE_COUNT = 60;
+    private GPBattleTask[] taskArray;
+    final List<String> sampleBots = new ArrayList<String>();
+    {
+        sampleBots.add( "RaikoMicro" );
+        sampleBots.add( "Komarious" );
+        sampleBots.add( "Dookious" );
+        sampleBots.add( "NanoAndrew" );
+        sampleBots.add( "ChumbaWumba" );
+        sampleBots.add( "AspidMovement" );
+        sampleBots.add( "SilverSurfer" );
+        sampleBots.add( "Freya" );
+        sampleBots.add( "Wodan" );
+        sampleBots.add( "Chiva" );
+        sampleBots.add( "Merkava" );
+        sampleBots.add( "Smoke" );
+        sampleBots.add( "Nimrod" );
+        sampleBots.add( "Smog" );
+        sampleBots.add( "Virgin" );
+        sampleBots.add( "Eve" );
+        sampleBots.add( "Evader" );
+        sampleBots.add( "Bandit" );
+        sampleBots.add( "Wolverine" );
+        sampleBots.add( "Sonda" );
+        sampleBots.add( "Hawkwing" );
+        sampleBots.add( "Terrible" );
+        sampleBots.add( "PerceptBot" );
+        sampleBots.add( "Predator" );
+        sampleBots.add("UnderDark");
+        sampleBots.add("Pasta");
+        sampleBots.add("DustBunny");
+        sampleBots.add("Ascendant");
+        sampleBots.add("Hyperion");
+        sampleBots.add("CrazyKitten");
+        sampleBots.add("Predator-2");
+        sampleBots.add("Movement");
+        sampleBots.add("NanoGod");
+        sampleBots.add("Ugluk");
+        sampleBots.add("Grumpy");
+        sampleBots.add("GloomyDark");
+        sampleBots.add("Mako");
+        sampleBots.add("Ali");
+        sampleBots.add("DarkFinal");
+        sampleBots.add("Fusion");
+        sampleBots.add("RamRod");
+        sampleBots.add("Mirror");
+        sampleBots.add("MirrorNano");
+        sampleBots.add("Squirtle");
+        sampleBots.add("Charon");
+        sampleBots.add("Pandora");
+        sampleBots.add("Squirrel");
+        sampleBots.add("TheArtOfWar");
+        sampleBots.add("Wisdom");
+    }
+
+
+    public FinalBotTester(JavaSpace space, String filename, TransactionManager transactionManager) {
+
+        this.space = space;
+        this.transactionManager = transactionManager;
+        try {
+            summaryWriter = new FileWriter(System.getProperty("user.home") + System.getProperty("file.separator") + "FinalBotTestSummary.log", true);
+            detailsWriter = new FileWriter(System.getProperty("user.home") + System.getProperty("file.separator") + "FinalBotTestDetails.log", true);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();  //Todo change body of catch statement use File | Settings | File Templates.
+        } catch (IOException e) {
+            e.printStackTrace();  //Todo change body of catch statement use File | Settings | File Templates.
+        }
+    }
+
+
+    public void testProgress(final List<Member> population, final int generation) {
+        List<Member> newPop = clonePopulation(population);
+        int battles = submitBattles(newPop, generation);
+        Map<Member, List<GPBattleResults>> results = collectResults(newPop, battles);
+        printResults(results);
+    }
+
+    private List<Member> clonePopulation(final List<Member> population) {
+        List<Member> newPop = new ArrayList<Member>(population.size());
+
+        for (Member m : population) {
+            String name = m.getName();
+            Member nm = new Member(m);
+            nm.setName(name);
+            newPop.add(nm);
+        }
+        return newPop;
+    }
+
+    private int submitBattles(List<Member> population, int generation) {
+
+        int numBattles = sampleBots.size() * population.size();
+        taskArray = new GPBattleTask[numBattles];
+        int battle = 0;
+        for (Member m : population) {
+            for (String s : sampleBots) {
+                GPBattleTask task = new GPBattleTask(generation, battle, m, s);
+                taskArray[battle] = task;
+                submitTask(task);
+                battle++;
+            }
+        }
+
+        return battle;
+    }
+
+    private Map<Member, List<GPBattleResults>> collectResults(List<Member> population, int numBattles) {
+        int takeCount = 0;
+        int retrieved = 0;
+        Entry template = new GPBattleResults();
+
+        Map<Member, List<GPBattleResults>> results = null;
+        results = new HashMap<Member, List<GPBattleResults>>();
+        for (Member m : population) {
+            results.put(m, new ArrayList<GPBattleResults>());
+        }
+
+        while (retrieved < numBattles) {
+            GPBattleResults res = null;
+            try {
+                while (res == null) {
+                    if (takeCount >= FinalBotTester.MAX_TAKE_COUNT) {
+                        resubmitTasks();
+                        takeCount = 0;
+                    }
+                    Transaction t = TransactionFactory.create(transactionManager, 30000).transaction;
+                    res = (GPBattleResults) space.takeIfExists(template, t, 0);
+                    if (res == null) {
+                        Thread.sleep(2000);
+                        takeCount++;
+                        t.commit();
+                    } else {
+                        takeCount = 0;
+                        t.commit();
+                        if (taskArray[res.battle] != null) {
+                            // not a duplicate
+                            taskArray[res.battle] = null;
+                        } else {
+                            // was a duplicate
+                            res = null;
+                        }
+                    }
+                }
+                logger.info(res.toString());
+            } catch (UnusableEntryException e) {
+                e.printStackTrace();
+            } catch (TransactionException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (RemoteException e) {
+                e.printStackTrace();
+            } catch (LeaseDeniedException e) {
+                e.printStackTrace();  //Todo change body of catch statement use File | Settings | File Templates.
+            }
+            if (res != null) {
+                String nameToUse;
+                if (sampleBots.contains(res.robot1)) {
+                    nameToUse = res.robot2;
+                } else {
+                    nameToUse = res.robot1;
+                }
+                for (Member m : results.keySet()) {
+                    if (m.getName().equals(nameToUse)) {
+                        List<GPBattleResults> l = results.get(m);
+                        l.add(res);
+                    }
+                }
+                logger.info("Collected " + retrieved + " of " + numBattles + " results");
+                ++retrieved;
+            }
+
+        }
+        return results;
+    }
+
+    private void submitTask(GPBattleTask task) {
+        try {
+            logger.info(task.shortString());
+            Transaction tran = TransactionFactory.create(transactionManager, 60000).transaction;
+            space.write(task, tran, Lease.FOREVER);
+            tran.commit();
+        } catch (TransactionException e) {
+            e.printStackTrace();
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        } catch (LeaseDeniedException e) {
+            e.printStackTrace();  //Todo change body of catch statement use File | Settings | File Templates.
+        }
+    }
+
+    private void resubmitTasks() {
+        for (GPBattleTask t : taskArray) {
+            if (t != null) {
+                try {
+                    if (space.readIfExists(t, null, 0) == null) {
+                        submitTask(t);
+                    }
+                } catch (UnusableEntryException e) {
+                    e.printStackTrace();
+                } catch (TransactionException e) {
+                    e.printStackTrace();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (RemoteException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }
+    }
+
+
+    private void printResults(Map<Member, List<GPBattleResults>> results) {
+        printDetails(results);
+        printSummary(results);
+    }
+
+    private void printDetails(Map<Member, List<GPBattleResults>> results) {
+        StringBuffer sb = new StringBuffer();
+
+        for (Map.Entry<Member, List<GPBattleResults>> e : results.entrySet()) {
+
+            for (GPBattleResults r : e.getValue()) {
+                sb.append(r.getSummary_CSV()).append('\n');
+            }
+        }
+        logger.info(sb.toString());
+        try {
+            detailsWriter.write(sb.toString());
+            detailsWriter.flush();
+        } catch (IOException e) {
+            e.printStackTrace();  //Todo change body of catch statement use File | Settings | File Templates.
+        }
+
+    }
+
+    private void printSummary(Map<Member, List<GPBattleResults>> results) {
+        StringBuffer sb = new StringBuffer();
+
+                int maxBeaten = 0;
+                int totalBeaten = 0;
+                int battles = 0;
+
+                for (Member m : results.keySet()) {
+                    int beaten = 0;
+                    for (GPBattleResults r : results.get(m)) {
+                        if (sampleBots.contains(r.robot2))
+                            if (r.score1 > r.score2
+                                    && r.firsts1 > r.firsts2
+                                    && r.seconds1 < r.seconds2
+                                    && r.fitness1 > 4.0)
+                                ++beaten;
+                        else
+                            if (r.score2 > r.score1
+                                    && r.firsts2 > r.firsts1
+                                    && r.seconds2 < r.seconds1
+                                    && r.fitness2 > 4.0)
+                                ++beaten;
+                        ++battles;
+                    }
+                    if (beaten > maxBeaten) maxBeaten = beaten;
+                    totalBeaten += beaten;
+                }
+                double wp = (double)totalBeaten / (double)battles;
+                sb.append(maxBeaten).append(',').append(wp).append('\n');
+                try {
+                    summaryWriter.write(sb.toString());
+                    summaryWriter.flush();
+                } catch (IOException e) {
+                    e.printStackTrace();  //Todo change body of catch statement use File | Settings | File Templates.
+                }
+
+    }
+
+}

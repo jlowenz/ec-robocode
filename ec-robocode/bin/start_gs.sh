3e0a2b2f601d4003a80f898fb1e6e3acd65aaf7f
ron1968 <ron1968@2b9fc8c8-3420-0410-9b65-e3934321462e>
2006-12-08 16:40:01 +0000

    Added a GP_GROUP variable

--- ec-robocode/bin/start_gs.sh
+++ ec-robocode/bin/start_gs.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+if [ -z ""{$GP_GROUP} ]
+then
+	echo "Setting group to GPRobocode"
+	GP_GROUP="GPRobocode"
+fi
 
-nice -n20 $GS_HOME/bin/gsServer.sh "/./space?groups=GPRobocode" "$GP_HOME/build/gp.jar" "-Dcom.gs.jini_lus.groups=GPRobocode" &
+nice -n20 $GS_HOME/bin/gsServer.sh "/./space?groups=${GP_GROUP}" "$GP_HOME/build/gp.jar" "-Dcom.gs.jini_lus.groups=${GP_GROUP}" &

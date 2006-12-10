46fc5a2b6335ff5c12121ac0789697aeaf7a4438
ron1968 <ron1968@2b9fc8c8-3420-0410-9b65-e3934321462e>
2006-12-10 04:14:13 +0000

    Removed win as factor added cell visitation bonus added unresponsiveness penalty

--- trunk/ec-robocode/gp/src/com/imaginaryday/ec/rcpatches/GPFitnessCalc.java
+++ trunk/ec-robocode/gp/src/com/imaginaryday/ec/rcpatches/GPFitnessCalc.java
@@ -14,6 +14,8 @@ public class GPFitnessCalc {
 	private static final double SURVIVAL_SCALE = 2.0;
     private static final double BULLET_PENALTY_SCALE = -1.0;
     private static final double MOVEMENT_PENALTY = -0.01;
+    private static final double UNRESPONDING_PENALTY_SCALE = 1000;
+    private static final double CELL_VISITING_SCALE = 3;
 
     private static double minBattleLength = Double.MAX_VALUE;
 
@@ -64,6 +66,58 @@ public class GPFitnessCalc {
 	    else doSomethingBonus = 0.0;
 
 	    return winningFraction * (damageFitness + tweekFitness) + doSomethingBonus;
+	}
+
+     private static double getLastFitness(int numGenerations, RobotResults robot, RobotResults opponent) {
+        // Add a bonus for accuracy
+	    double accuracy;
+	    if (robot.getNumBulletsFired() > 0) {
+	        accuracy = (double) robot.getNumBulletHits() / (double) robot.getNumBulletsFired();
+	    } else {
+	        accuracy = 0;
+	    }
+	    double accuracyBonus = ACCURACY_SCALE * accuracy * robot.getBulletDamage();
+
+	    /*
+	     * Add a bonus for scan efficiency.
+	     * Efficiency is getting one scan even per battle "tick"
+	     */
+	    double scanEfficiency;
+	    if (robot.getNumScanEvents() > 0 && robot.getBattleLength() > 0) {
+	        double revs = robot.getBattleLength();
+	        scanEfficiency = (double) robot.getNumScanEvents() / revs;
+	    } else {
+	        scanEfficiency = 0.0;
+	    }
+	    double scanEfficiencyBonus = EFFICIENCY_SCALE * scanEfficiency * robot.getBulletDamage();
+
+        double coverageFraction;
+        if (robot.getCellsEntered() > 0) {
+            coverageFraction = robot.getCellsEntered() / 48;
+	    } else {
+	        coverageFraction = 0;
+	    }
+	    double motionBonus = coverageFraction * robot.getBulletDamage() * CELL_VISITING_SCALE;
+
+        double damageFitness = robot.getBulletDamage() + robot.getBulletDamageBonus() + robot.getRamDamage()
+	            + robot.getRamDamageBonus() + accuracyBonus + scanEfficiencyBonus + motionBonus;
+
+	    double d = (robot.getDistanceTravelled() < 1) ? 1.0 : robot.getDistanceTravelled();
+	    double b = (robot.getNumBulletsFired() < 1) ? 1.0 : robot.getNumBulletsFired();
+	    double s = (robot.getNumScanEvents() < 1) ? 1.0 : robot.getNumScanEvents();
+
+	    double tweekFitness = Math.log(d) + Math.log(b) + Math.log(s);
+
+	    double winningFraction = (double) robot.getFirsts() / 10.0;
+	    double doSomethingBonus;
+	    if ((robot.getDistanceTravelled() > 0 ||
+	            robot.getNumBulletsFired() > 0 ||
+	            robot.getNumScanEvents() > 0)) doSomethingBonus = 5.0;
+	    else doSomethingBonus = 0.0;
+
+        double unrespondingPenalty = (robot.getMaxUnrespondedHits() - 1) * UNRESPONDING_PENALTY_SCALE;
+
+        return damageFitness + tweekFitness + doSomethingBonus - unrespondingPenalty;
 	}
 
 
